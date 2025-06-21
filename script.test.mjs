7239a117e7841901a019cec759ef5acae06902bd
Droid-An <andrmail6@gmail.com>
2025-06-21 20:13:09 +0100

    added more test cases

--- script.test.mjs
+++ script.test.mjs
@@ -3,28 +3,44 @@ import assert from "node:assert";
 import test from "node:test";
 import words from "./words.json" with { type: "json" };
 
-// he go to the island
-// they make a fire
-// he will get married, she will not
-// red-orange fire
-// I love Glasgow
-
-test("check text", () => {
+test("he go to the island", () => {
   assert.deepEqual(checkText("he go to the island"), []);
 });
 
-test("check text", () => {
+test("they make a fire", () => {
   assert.deepEqual(checkText("they make a fire"), []);
 });
 
-test("check text", () => {
+test("he will get married, she will not", () => {
   assert.deepEqual(checkText("he will get married, she will not"), []);
 });
 
-test("check text", () => {
+test("red-orange fire", () => {
   assert.deepEqual(checkText("red-orange fire"), []);
 });
 
-test("check text", () => {
+test("I love Glasgow", () => {
   assert.deepEqual(checkText("I love Glasgow"), []);
+});
+
+// hello world
+// they create some dinner
+// she give gift, then go out
+// he like egg-nog
+// go to birmingham
+
+test("hello world", () => {
+  assert.deepEqual(checkText("hello world"), ["hello", "world"]);
+});
+test("they create some dinner", () => {
+  assert.deepEqual(checkText("they create some dinner"), ["create", "dinner"]);
+});
+test("she give gift, then go out", () => {
+  assert.deepEqual(checkText("she give gift, then go out"), ["gift"]);
+});
+test("he like egg-nog", () => {
+  assert.deepEqual(checkText("he like egg-nog"), ["nog"]);
+});
+test("go to birmingham", () => {
+  assert.deepEqual(checkText("go to birmingham"), ["birmingham"]);
 });
