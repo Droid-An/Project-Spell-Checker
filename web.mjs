27f4e895f0f70267395cc3deeef3110ae67d1bd7
Droid-An <andrei.filippov.aaf@gmail.com>
2025-06-23 18:42:57 +0100

    removed onload function

--- web.mjs
+++ web.mjs
@@ -5,9 +5,3 @@ checkButton.addEventListener("click", (e) => {
   e.preventDefault();
   handleInput();
 });
-
-// window.onload = function () {
-//   document.querySelector(
-//     "body"
-//   ).innerText = `There are ${getDictionarySize()} words in the Basic English dictionary`;
-// };
