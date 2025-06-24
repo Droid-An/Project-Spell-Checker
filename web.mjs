4c146b9733e047c1cdbea445e4c34d3bc76d6993
Droid-An <andrmail6@gmail.com>
2025-06-24 18:26:45 +0100

    now check runs on ctrl enter

--- web.mjs
+++ web.mjs
@@ -7,7 +7,20 @@ checkButton.addEventListener("click", (e) => {
 });
 
 const textInputField = document.querySelector("#textInput");
-textInputField.addEventListener("keyup", () => {
+const listOfMistakes = document.querySelector("#listOfMistakes");
+const mistakesMessage = document.querySelector("#mistakesMessage");
+
+textInputField.addEventListener("keydown", (e) => {
+  // Prevent clearing output after Ctrl+Enter
+  if (e.ctrlKey && e.key === "Enter") return;
+
   listOfMistakes.innerHTML = "";
   mistakesMessage.style.display = "none";
+});
+
+textInputField.addEventListener("keyup", (e) => {
+  if (e.ctrlKey && e.key === "Enter") {
+    e.preventDefault(); // stop new line
+    handleInput(); // simulate form submit
+  }
 });
